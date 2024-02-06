5cc88b891649f0674b11376fd2adb3da7bbafe7a
Paden O'Neal <141882208+Lagor845@users.noreply.github.com>
2024-02-06 12:58:18 -0700

    Delete Client directory

--- Client/Gui/networking.py
+++
@@ -1,8 +0,0 @@
-import socket
-from threading import Thread
-
-class client():
-    def __init__(self) -> None:
-        self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.Ip_address = ""
-        self.Port = 34051
